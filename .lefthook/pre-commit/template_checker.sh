5238f93b605514e2a52d5f212b3214bac5ed136b
Manvir <manvirheer007@gmail.com>
2025-05-17 18:10:01 -0700

    bad commit text

--- .lefthook/pre-commit/template_checker.sh
+++ .lefthook/pre-commit/template_checker.sh
@@ -2,6 +2,11 @@ INPUT_FILE=$1
 START_LINE=`head -n1 $INPUT_FILE`
 PATTERN="^(TICKET)-[[:digit:]]+: "
 if ! [[ "$START_LINE" =~ $PATTERN ]]; then
+
   echo "Bad commit message, see example: TICKET-123: some text"
   exit 1
+## if else print the message
+else
+  echo "Good commit message"
+  exit 0
 fi
